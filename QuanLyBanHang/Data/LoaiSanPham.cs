cc040ec65f527a4535e2dd4eed985fe224052138
Nguyễn Văn Trọng Nộp Bài <trong_dth235830@student.agu.edu.vn>
2026-01-21 23:11:27 +0700

    Add files via upload

---
+++ QuanLyBanHang/Data/LoaiSanPham.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHang.Data
+{
+    public class LoaiSanPham
+    {
+        public int ID { get; set; }
+        public string TenLoai { get; set; }
+        public virtual ObservableCollectionListSource<SanPham> SanPham { get; } = new();
+    }
+}
